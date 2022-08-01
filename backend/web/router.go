ae2666b43b07b0b3b55f6f47b72d7211631d1810
Mars Zuo <marszuo@foxmail.com>
2022-08-01 08:48:33 +0800

    feat: configure static page routing

--- backend/web/router.go
+++ backend/web/router.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type Router struct {
@@ -34,5 +36,31 @@ func (r *Router) registerHandler() {
 	r.Handle("/api/page", r.ch.ThenFunc(AddPage)).Methods("POST")
 	r.Handle("/api/page", r.ch.ThenFunc(DeletePage)).Methods("DELETE")
 	// static page
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./dist"))))
+	r.PathPrefix("/").Handler(vueRouter("/", http.FileServer(http.Dir("./dist"))))
+}
+
+func vueRouter(prefix string, h http.Handler) http.Handler {
+	if prefix == "" {
+		return h
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p := strings.TrimPrefix(r.URL.Path, prefix)
+		// 适配Vue路由
+		switch p {
+		case "datasource", "page", "datatable":
+			p = ""
+		}
+		rp := strings.TrimPrefix(r.URL.RawPath, prefix)
+		if len(p) < len(r.URL.Path) && (r.URL.RawPath == "" || len(rp) < len(r.URL.RawPath)) {
+			r2 := new(http.Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r.URL
+			r2.URL.Path = p
+			r2.URL.RawPath = rp
+			h.ServeHTTP(w, r2)
+		} else {
+			http.NotFound(w, r)
+		}
+	})
 }
